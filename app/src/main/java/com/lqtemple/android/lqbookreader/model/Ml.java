09d3205db901e49226a3efc939c60cbee8afe8e0
sundxing <sundxing@yeah.net>
2017-03-12 15:25:45 +0800

    add spain module

---
+++ app/src/main/java/com/lqtemple/android/lqbookreader/model/Ml.java
@@ -0,0 +1,16 @@
+package com.lqtemple.android.lqbookreader.model;
+
+import java.util.List;
+
+import static android.os.Build.VERSION_CODES.M;
+
+/**
+ * Created by sun on 2017/3/12.
+ */
+
+public class Ml {
+    String title;
+    String url;
+    String path;
+    int depth;
+}
